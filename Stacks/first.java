20ac89f0e53ac2c9c96d41651aaeb72861f127c8
Ujjaval Goyal <ujjavalgoyal5@gmail.com>
2026-04-09 11:47:19 +0530

    ujjaval

---
+++ Stacks/first.java
@@ -0,0 +1,20 @@
+
+import java.util.Stack;
+
+public class first{
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack<>();
+        st.push(34);
+        st.push(45);
+        st.push(2);
+        st.push(19);
+        st.push(18);
+        System.out.println(st.pop());
+        System.out.println(st.pop());
+        System.out.println(st.pop());
+        System.out.println(st.pop());
+        System.out.println(st.pop());
+        System.out.println(st.pop());
+
+    }
+}
